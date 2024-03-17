f8386c92d89eb30fe30d393b85239ab01230f10a
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-17 16:16:42 +0330

    add outlet

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,14 @@
-import { ReactNode } from "react";
 import Header from "../Header";
 import SideBar from "../SideBar";
+import { Outlet } from "react-router-dom";
 
-type layoutProps = {
-  children: ReactNode;
-};
-
-export default function Layout({ children }: layoutProps) {
+export default function Layout() {
   return (
     <div className="flex justify-center h-full">
       <div className=" w-[1240px] h-full">
         <Header />
         <div className="flex justify-between gap-x-3  h-full mt-6">
-          {children}
+          <Outlet />
           <SideBar />
         </div>
       </div>
