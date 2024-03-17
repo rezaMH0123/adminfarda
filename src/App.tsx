eddc4b8eb96010655dcb30f334f6b9836f54dc2f
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-17 13:20:17 +0330

    add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Content from "./pages/Content";
+import Comment from "./pages/Comment";
+import ManageFiles from "./pages/ManageFiles";
+import Categories from "./pages/Categories";
+import Labels from "./pages/Labels";
+
 function App() {
   return (
-    <>
-      <div className="border border-red-600">heloooooooooo</div>
-    </>
+    <div className="border border-red-600 h-screen px-12 py-4 overflow-hidden ">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/manageFiles" element={<ManageFiles />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/labels" element={<Labels />} />
+        </Routes>
+      </Layout>
+    </div>
   );
 }
 
