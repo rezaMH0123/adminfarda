befa20e0108ac2461a38af6df49f9716382dfa0c
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-17 13:21:21 +0330

    add box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    boxShadow: {
+      custom1: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;",
+    },
     extend: {},
   },
   plugins: [],
