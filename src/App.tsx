65c4a32d12057c4da68a7b7a2630f0a2becf6270
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-17 16:15:27 +0330

    fixd the layout of routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,25 @@ import Comment from "./pages/Comment";
 import ManageFiles from "./pages/ManageFiles";
 import Categories from "./pages/Categories";
 import Labels from "./pages/Labels";
+import { useState } from "react";
+import SignIn from "./pages/auth/SignIn";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  // const [isLogin, setIsLoging] = useState(true);
   return (
     <div className="border border-red-600 h-screen px-12 py-4 overflow-hidden ">
-      <Layout>
-        <Routes>
+      <Routes>
+        <Route element={<Layout />}>
           <Route path="/" element={<Content />} />
           <Route path="/comment" element={<Comment />} />
           <Route path="/manageFiles" element={<ManageFiles />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/labels" element={<Labels />} />
-        </Routes>
-      </Layout>
+        </Route>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
