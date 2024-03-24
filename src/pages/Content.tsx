f0dbe7aedca63012c4d9c3022fece48118f759ee
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-24 10:53:26 +0330

    build additionButton to Common components

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,10 +1,24 @@
-import { Outlet } from "react-router-dom";
+import plusIcon from "../assets/img/tools/plus.svg";
+import filrterIcon from "../assets/img/tools/filter.svg";
+import AdditionButton from "../components/Common/additionButton";
 
 export default function Content() {
   return (
-    <div className="border border-green-300 w-[80%] h-[70%] ">
-      content
-      <Outlet />
+    <div
+      dir="rtl"
+      className="border border-[#E1E1E1] w-[80%] h-[75%] rounded-xl "
+    >
+      <div className="header flex justify-between items-center px-6 border border-red-500 h-[15%] ">
+        <div className="right">
+          <span className="text-[20px] font-medium">محتوا</span>
+        </div>
+        <div className="left flex items-center border border-green-400 w-[30%] h-full">
+          <AdditionButton className="w-[152px] h-[44px] ">
+            <span className="text-[14px]">اضافه کردن</span>
+            <img src={plusIcon} alt="plusIcon" />
+          </AdditionButton>
+        </div>
+      </div>
     </div>
   );
 }
