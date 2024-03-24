80430474783dfd856f4c0cb34b294cd54c1bd370
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-24 10:52:21 +0330

    add colors to extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     boxShadow: {
       custom1: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;",
     },
-    extend: {},
+
+    extend: {
+      colors: {
+        PrimaryBlue: {
+          100: "#33BDF1",
+          200: "#19B6F1",
+          300: "#00ABEB",
+        },
+      },
+    },
   },
   plugins: [],
 };
