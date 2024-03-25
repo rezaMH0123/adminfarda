07908533853949a79332063cdd1deda39f209181
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-25 15:22:44 +0330

    build pagination component

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,9 +1,19 @@
 import plusIcon from "../assets/img/tools/plus.svg";
 import filrterIcon from "../assets/img/tools/filter.svg";
 import AdditionButton from "../components/Common/additionButton";
-import IconChevron from "../components/Icons/Chevron";
+import Pagination from "../components/Common/pagination";
+import { useState } from "react";
 
 export default function Content() {
+  const [allPage, setAllPage] = useState<number>(8);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const nextPageClick = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+  const prevPageClick = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
   return (
     <div
       dir="rtl"
@@ -11,21 +21,23 @@ export default function Content() {
     >
       <div className="header flex justify-between items-center px-6 h-[15%] ">
         <div className="right">
-          <span className="text-[20px] font-medium">محتوا</span>
+          <span className="text-[20px] font-ShabnamMedium font-semibold">
+            محتوا
+          </span>
         </div>
         <div className="left flex justify-end items-center gap-x-5  w-[30%] h-full">
-          <div className="flex justify-center items-center cursor-pointer">
+          <div className="flex justify-center items-center cursor-pointer font-ShabnamMedium">
             <span className="text-PrimaryBlack-300">فیلتر</span>
             <img src={filrterIcon} alt="filrterIcon" />
           </div>
-          <AdditionButton className="w-[152px] h-[44px] ">
+          <AdditionButton className="w-[152px] h-[44px] font-ShabnamMedium ">
             <span className="text-[14px]">اضافه کردن</span>
             <img src={plusIcon} alt="plusIcon" />
           </AdditionButton>
         </div>
       </div>
       <div className="centerContetnt px-6 h-full">
-        <div className="headChart flex border-b border-PrimaryBlack-300 h-[60px]">
+        <div className="headChart flex border-b border-PrimaryBlack-300 h-[60px] font-ShabnamMedium">
           <div className="flex items-center w-[40%]">
             <span className="mr-5">عنوان</span>
           </div>
@@ -44,24 +56,17 @@ export default function Content() {
         </div>
 
         <div className="border-b border-PrimaryBlack-300 h-[57%] ">
-          {/* {Array(6).map((item, index) => (
-            <div className="border border-red-600 h-[10px]"></div>
-          ))} */}
           {[...Array(6)].map((item) => (
             <div className="h-[15%] mb-1 border border-red-600"></div>
           ))}
         </div>
-        <div className="flex gap-x-5 justify-end items-center  h-[15%] text-[#404040]">
-          <div>
-            <span className="text-base">
-              نمایش آیتم ها از {1} تا {4}
-            </span>
-          </div>
-          <button className="flex justify-center items-center gap-x-3 border border-[#BABABA] w-[163px] rounded-xl py-2 px-2 ">
-            <IconChevron className="ml-1" fill="#33BDF1" />
-            <span>صفحه بعد</span>
-            <span>1</span>
-          </button>
+        <div className="flex gap-x-5 justify-end items-center  h-[15%]">
+          <Pagination
+            allPage={allPage}
+            currentPage={currentPage}
+            nextPageClick={nextPageClick}
+            prevPageClick={prevPageClick}
+          />
         </div>
       </div>
     </div>
