905872858f61737810c573376641508a85571500
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-26 14:19:28 +0330

    build modal component

---
+++ src/components/Common/modal.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, MouseEvent } from "react";
+import "animate.css";
+
+type ModalProps = {
+  children: ReactNode;
+  width: number;
+  height: number;
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function Modal({
+  children,
+  width,
+  height,
+  setOpenModal,
+}: ModalProps) {
+  const handleModalClick = (event: MouseEvent<HTMLDivElement>) => {
+    // Prevent the click event from propagating to the backdrop click handler
+    event.stopPropagation();
+  };
+
+  return (
+    <div
+      onClick={() => setOpenModal(false)}
+      className="absolute top-0 left-0 flex justify-center items-center  bg-black w-full h-full bg-opacity-60"
+    >
+      <div
+        onClick={handleModalClick}
+        className={`animate__animated  animate__fadeInDown flex justify-center items-center bg-white w-[${width}%] h-[${height}%] rounded-lg`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
