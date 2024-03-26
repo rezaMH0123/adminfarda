81f7a8c0617e7e610d18a05b2b7800890e4babfe
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-03-26 14:19:56 +0330

    for content page modal body

---
+++ src/components/ContentsModalBody/index.tsx
@@ -0,0 +1,15 @@
+export default function ContentsModalBody() {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="border border-red-500 h-full w-[90%] py-4">
+        <div className="border border-green-600 h-[25%]"></div>
+        <div className="border border-green-600 h-[25%]"></div>
+        <div className="border border-green-600 h-[10%]"></div>
+        <div className="border border-green-600 h-[10%]"></div>
+        <div className="border border-green-600 h-[10%]"></div>
+        <div className="border border-green-600 h-[10%]"></div>
+        <div className="border border-green-600 h-[10%]"></div>
+      </div>
+    </div>
+  );
+}
